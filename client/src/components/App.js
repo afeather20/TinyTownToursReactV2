b829509bcb8c9a544acb175a356dfa2459792e12
Adam Feather <afeather@thinklogic.net>
2020-09-22 12:10:21 -0700

    all files

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,10 @@
 import React, { Component, useState } from "react";
 import logo from "../logo.svg";
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { Switch, Route, Link } from "react-router-dom";
 
 import HomePage from './homePage/homePage';
+import AdamPage from './AdamPage/adamPage';
 import PortNavbar from './navbar/Navbar';
 
 class App extends Component {
@@ -50,7 +51,10 @@ class App extends Component {
         return (
             <div className="App wrapper">
               <PortNavbar toggle={this.toggle} isOpen={this.state.isOpen} />
-              <HomePage toggle={this.toggle} isOpen={this.state.isOpen} />
+              <Switch>
+                <Route exact path="/HomePage" component={HomePage} />
+                <Route exact path="/AdamPage" component={AdamPage}/>
+              </Switch>
             </div>
           );
     }
